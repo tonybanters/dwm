071dfc14fae4fafad0b0dc0ad3cc486151dc2d5c
tonybanters <tonybanters@gmail.com>
2025-04-10 01:53:59 -0700

    updated background color.

--- config.h
+++ config.h
@@ -14,13 +14,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=16" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=16";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
+/*static const char col_gray1[]       = "#222222";*/
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-/*static const char col_cyan[]        = "#005577";*/
-/*static const char col_cyan[]        = "#0b104f";*/
-/*static const char col_cyan[]        = "#6dade3";*/
 static const char col_cyan[]        = "#4d6a8e";
 
 static const char *colors[][3]      = {
